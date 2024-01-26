cf19f1e59601a62df12e70dd2dc5f944d0bed25a
Ata Safapour <safapour.ata@gmail.com>
2024-01-26 04:50:01 +0330

    feat: added admin page & admin permissions for it

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,17 @@
 from flask import Flask
 from flask_security import SQLAlchemyUserDatastore
 
-from app.extensions import db, migrate, security
+from app.extensions import db, migrate, security, admin, MyAdminIndexView, MyModelView
 from app.users.routes import blueprint as users_blueprint
 from app.posts.routes import blueprint as posts_blueprint
 
 def register_blueprints(app):
     app.register_blueprint(users_blueprint)
     app.register_blueprint(posts_blueprint)
+
+def add_view_to_admin_panel(admin):
+    admin.add_view(MyModelView(User, db.session))
+    admin.add_view(MyModelView(Role, db.session))
 
 app = Flask(__name__)
 register_blueprints(app)
@@ -20,3 +24,6 @@ migrate.init_app(app, db, render_as_batch=True)
 
 user_datastore = SQLAlchemyUserDatastore(db, User, Role)
 security.init_app(app, user_datastore)
+
+admin.init_app(app, index_view=MyAdminIndexView())
+add_view_to_admin_panel(admin)
--- app/extensions.py
+++ app/extensions.py
@@ -1,9 +1,23 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from flask_security import Security
+from flask_security import Security, current_user
+from flask_admin import Admin, AdminIndexView
+from flask_admin.contrib.sqla import ModelView
 
 
 db = SQLAlchemy()
 migrate = Migrate()
 security = Security()
+admin = Admin()
 
+class MyAdminIndexView(AdminIndexView):
+    def is_accessible(self):
+        print(current_user.roles)
+        return current_user.has_role('admin')
+
+class MyModelView(ModelView):
+    def is_accessible(self):
+        return current_user.has_role('admin')
+    
+    def inaccessible_callback(self, name, **kwargs):
+        return super().inaccessible_callback(name, **kwargs)
